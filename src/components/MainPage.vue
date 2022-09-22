a5748d497b5b6a2788eccd7312a71654c1008cd7
BuildTools <unconfigured@null.spigotmc.org>
2022-09-22 22:04:50 +0200

    Add Main Page Component

---
+++ src/components/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+        This is main page.
+    </div>
+</template>
+
+<script>
+export default {
+	name: "MainPage",
+};
+</script>
+
+<style lang="sass" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<main>
+		<div class="wrapper">
+			<MainPage />
+		</div>
+	</main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MainPage from "@/components/MainPage.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+	name: "HomeView",
+	components: {
+		MainPage,
+	},
+};
 </script>
