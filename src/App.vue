8b09c7845a015e8bf74691556c4fc7ae4ee0e849
BuildTools <unconfigured@null.spigotmc.org>
2022-09-23 12:41:38 +0200

    Nav Bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavBar />
   <router-view/>
 </template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+	components: {
+		NavBar,
+	},
+};
+</script>
 
 <style>
 *,
@@ -32,18 +39,5 @@ html {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
