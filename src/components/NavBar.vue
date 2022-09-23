f74c991f93173d9e1d749a159c6ac1cc0048aa36
BuildTools <unconfigured@null.spigotmc.org>
2022-09-23 15:24:36 +0200

    Nav Item

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,27 +1,51 @@
 <template>
     <nav class="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
+        <div class="nav__logo">
+            <img src="@/assets/logo.png"/>
+            <p class="nav__title">Animals Living in the ZOO</p>
+        </div>
+        <NavBarItem :item="{title:'Home',to:'/'}"/>
+        <NavBarItem :item="{title:'About',to:'/about'}"/>
     </nav>
 </template>
 
 <script>
+import NavBarItem from "@/components/NavBarItem.vue";
 export default {
     name: "NavBar",
+    components:{
+        NavBarItem,
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-nav {
-  padding: 30px;
-}
+.nav {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    position: sticky;
+    top: 0;
+    padding: 0 2em;
+    height: 60px;
+    background-color: white;
+    box-shadow: 5px 0 5px #000;
+    z-index: 1000;
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    &__logo{
+        display: flex;
+        align-items: center;
+        justify-self: start;
+        margin-right: auto;
 
-nav a.router-link-exact-active {
-  color: #42b983;
+        img{
+            width: 30px;
+            margin-right: 1em;
+        }
+    }
+    &__title{
+        font-size: 1.6rem;
+        width: 50%;
+    }
 }
 </style>
---
+++ src/components/NavBarItem.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="nav__links">
+		<router-link :to="item.to">{{ item.title }}</router-link>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "NavBarItem",
+	props: {
+		item: {
+			type: Object,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+    &__links{
+        display: flex;
+        align-items: center;
+        padding: 1em;
+        height:100%;
+        font-size: 1.8rem;
+        transition: background-color 0.5s;
+        &:hover{
+            background-color: #42b983;
+            a, a.router-link-exact-active{
+                color: #fff;
+            }
+            
+        }
+    }
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+        text-decoration: none;
+        letter-spacing: 2px;
+        transition: color 0.5s;
+    }
+
+    a.router-link-exact-active {
+        color: #42b983;
+        transition: color 0.5s;
+    }
+}
+</style>
