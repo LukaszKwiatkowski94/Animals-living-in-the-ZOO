b7caf71d37f922f30aa5327e43649eec233bd867
BuildTools <unconfigured@null.spigotmc.org>
2023-09-23 11:02:56 +0200

    Data from JSON

--- src/components/ListAnimals.vue
+++ src/components/ListAnimals.vue
@@ -1,23 +1,42 @@
 <template>
     <div class="animals">
-        <SingleAnimal v-for="animal in animals" :key="animal.id" :animal="animal">{{animal.name}}</SingleAnimal>
+        <SingleAnimal v-for="animal in randomAnimals" :key="animal.id" :animal="animal">{{animal.name}}</SingleAnimal>
     </div>
 </template>
 
 <script>
     import { ref } from 'vue';
     import SingleAnimal from "@/components/SingleAnimal.vue";
+    import animalsData from "@/assets/animals.json";
     export default {
         name: "ListAnimals",
         components:{
             SingleAnimal,
         },
-        async setup() {
-            let animals = ref(null);
-            // let data = await fetch('https://zoo-animal-api.herokuapp.com/animals/rand/9'); // The old api
-            let data = await fetch('http://animals-api.portfolio-lukasz-kwiatkowski.cba.pl/api/animals-random');
-            animals = await data.json();       
-            return {animals,}
+    async setup() {
+            const animals = ref([]);
+            const randomAnimals = ref([]);
+
+            const getRandomAnimals = () => {
+                const shuffled = animals.value.sort(() => 0.5 - Math.random());
+                randomAnimals.value = shuffled.slice(0, 9);
+                const updatedAnimals = randomAnimals.value.map(animal => {
+                    return {
+                    ...animal,
+                    image_link: "/animals/"+animal.image_link  // Zaktualizuj ten adres zgodnie z wymaganiami
+                    };
+                });
+
+                randomAnimals.value = updatedAnimals;
+            };
+
+            // Ustawienie danych z pliku JSON jako referencję
+                animals.value = animalsData;
+
+            // Pobranie losowych zwierząt i obcięcie do 9 rekordów
+                getRandomAnimals();
+
+                return { randomAnimals };
         },
     };
 </script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,21 +1,45 @@
 <template>
-  <CustomHeader :text="namePageHeader"/>
-  <main class="about">
-    <div class="wrapper">
-      <section class="section">
-        <h2 class="section__header">What is this app?</h2>
-        <p class="section__text">This app is part of a portfolio. It was based on the VueJs framework. Its home page displays information about 9 random animals living in the zoo. Data for this application comes from an external API. The link is below.</p>
-      </section>
-      <section class="section">
-        <h2 class="section__header">Links</h2>
-        <ul class="section__list">
-          <!--<li class="section__list-item"><a href="https://zoo-animal-api.herokuapp.com/" class="section__link" target=”_blank”>Zoo Animal API</a></li>-->
-          <li class="section__list-item"><a href="http://animals-api.portfolio-lukasz-kwiatkowski.cba.pl/" class="section__link" target=”_blank”>Animals API - My Project</a></li>
-          <li class="section__list-item"><a href="https://github.com/LukaszKwiatkowski94/Animals-living-in-the-ZOO" class="section__link" target=”_blank”>Application code on GitHub</a></li>
-        </ul>
-      </section>
-    </div> 
-  </main>
+	<CustomHeader :text="namePageHeader" />
+	<main class="about">
+		<div class="wrapper">
+			<section class="section">
+				<h2 class="section__header">What is this app?</h2>
+				<p class="section__text">
+					This application is currently part of my portfolio. It is based on the
+					VueJs framework, which allows for responsive and interactive web
+					design. The main goal of this application is to present information
+					about 9 random animals living in the zoo. The data about these animals
+					is dynamically loaded from a JSON file.
+					<br />
+					<br />
+					To view the code of this application and familiarize yourself with its
+					structure, I invite you to visit my repository on GitHub: 
+          <a
+							href="https://github.com/LukaszKwiatkowski94/Animals-living-in-the-ZOO"
+							class="section__link"
+							target="”_blank”"
+							>
+          link to the GitHub repository
+          </a>
+          . There you will find the full source code,
+					documentation, and the opportunity to explore the project.
+				</p>
+			</section>
+			<section class="section">
+				<h2 class="section__header">Links</h2>
+				<ul class="section__list">
+					<li class="section__list-item">
+						<a
+							href="https://github.com/LukaszKwiatkowski94/Animals-living-in-the-ZOO"
+							class="section__link"
+							target="”_blank”"
+							>Application code on GitHub</a
+						>
+					</li>
+				</ul>
+			</section>
+		</div>
+	</main>
 </template>
 
 <script>
@@ -26,40 +50,40 @@ export default {
 	components: {
 		CustomHeader,
 	},
-  data(){
-    return{
-      namePageHeader:"About",
-    }
-  }
+	data() {
+		return {
+			namePageHeader: "About",
+		};
+	},
 };
 </script>
 
 <style lang="scss" scoped>
-  .about{
-    margin-top: 2em;
-    min-height: calc(100vh - 120px - 2em - 10em);
-    text-align: left;
-  }
-  .section{
-    margin: 2em auto;
-    padding: 2em;
-    &__header{
-      margin-bottom: 0.5em;
-      font-size: 2rem;
-    }
-    &__text{
-      text-align: justify;
-      font-size: 1.6rem;
-    }
-    &__list{
-      margin-left: 2em;
-    }
-    &__list-item{
-      font-size: 1.6rem;
-    }
-    &__link{
-      text-decoration: none;
-      color: #42b983;
-    }
-  }
+.about {
+	margin-top: 2em;
+	min-height: calc(100vh - 120px - 2em - 10em);
+	text-align: left;
+}
+.section {
+	margin: 2em auto;
+	padding: 2em;
+	&__header {
+		margin-bottom: 0.5em;
+		font-size: 2rem;
+	}
+	&__text {
+		text-align: justify;
+		font-size: 1.6rem;
+	}
+	&__list {
+		margin-left: 2em;
+	}
+	&__list-item {
+		font-size: 1.6rem;
+	}
+	&__link {
+		text-decoration: none;
+		color: #42b983;
+	}
+}
 </style>
